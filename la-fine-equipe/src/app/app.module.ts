fa9fcb2791d5be5d13f89a2cf22c4b058f92d20a
Fanny <fannyeber@gmail.com>
2023-10-24 14:45:02 +0200

    create table for intervention and routerLink to details

--- la-fine-equipe/src/app/app.module.ts
+++ la-fine-equipe/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailComponent } from './detail/detail.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingListComponent } from './landing-list/landing-list.component';
 
@@ -15,7 +14,6 @@ import { LandingListComponent } from './landing-list/landing-list.component';
     LandingListComponent,
     DetailComponent,
     HeaderComponent,
-    HomeComponent,
     FooterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
--- la-fine-equipe/src/app/landing-list/landing-list.component.ts
+++ la-fine-equipe/src/app/landing-list/landing-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Intervention } from '../model/intervention';
 
 @Component({
   selector: 'app-landing-list',
   templateUrl: './landing-list.component.html',
-  styleUrls: ['./landing-list.component.scss']
+  styleUrls: ['./landing-list.component.scss'],
 })
-export class LandingListComponent {
+export class LandingListComponent implements OnInit {
+  passedInterventions: Intervention[];
+  futureInterventions: Intervention[];
 
+  ngOnInit(): void {
+    //TODO récupérer les interventions passé et à venir
+    var intervention = new Intervention();
+    intervention.date = new Date();
+    intervention.id = '1';
+    intervention.intervention = 'scanner';
+    intervention.isConfirmed = true;
+    intervention.isPayed = false;
+    intervention.place = '3 rue ALice Guy';
+    this.passedInterventions = [intervention, intervention];
+    this.futureInterventions = [intervention];
+  }
+
+  confirmIntervention(interventionId: string) {
+    //TODO confirmer l'intervention à venir (identificable par interventionId)
+  }
+
+  payIntervention(interventionId: string) {
+    //TODO payer l'intervention passé (identificable par interventionId)
+  }
 }
